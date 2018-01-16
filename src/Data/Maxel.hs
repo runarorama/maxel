249cc562dd433cad5b96aa46683b4dda265f3426
Runar Bjarnason <runarorama@gmail.com>
2018-01-15 20:50:27 -0500

    Size and extent

--- src/Data/Maxel.hs
+++ src/Data/Maxel.hs
@@ -1,13 +1,16 @@
 module Data.Maxel where
 
-import Control.Applicative
-import Data.MultiSet
+import           Control.Applicative
+import qualified Data.MultiSet as S
+import           Numeric.Natural
 
 data Pixel a = Pixel { row :: a, column :: a }
   deriving (Eq, Ord, Show)
 
-newtype Maxel a = Maxel { unMaxel :: MultiSet (Pixel a) }
+newtype Maxel a = Maxel { unMaxel :: S.MultiSet (Pixel a) }
   deriving (Eq, Ord, Show)
+
+---- Pixels ----
 
 diagonal :: a -> Pixel a
 diagonal a = Pixel a a
@@ -23,4 +26,13 @@ rowCollinear (Pixel r1 _) (Pixel r2 _) = r1 == r2
 
 collinear :: (Eq a) => Pixel a -> Pixel a -> Bool
 collinear = liftA2 (||) <$> columnCollinear <*> rowCollinear
+
+---- Maxels ----
+
+size :: Maxel a -> Natural
+size (Maxel s) = fromIntegral $ S.size s
+
+extent :: Ord a => Maxel a -> Pixel a
+extent (Maxel s) =
+  Pixel (S.findMax $ S.map row s) (S.findMax $ S.map column s)
 
