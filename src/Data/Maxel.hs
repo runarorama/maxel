d66a13c625b97f79a1af82b79fca3bdad56edaf1
Runar Bjarnason <runarorama@gmail.com>
2018-01-15 20:22:38 -0500

    Testing with Hedgehog

--- src/Data/Maxel.hs
+++ src/Data/Maxel.hs
@@ -1,9 +1,25 @@
 module Data.Maxel where
 
+import Control.Applicative
 import Data.MultiSet
 
-data Pixel a = Pixel { pixelX :: a, pixelY :: a }
+data Pixel a = Pixel { row :: a, column :: a }
+  deriving (Eq, Ord, Show)
 
 newtype Maxel a = Maxel { unMaxel :: MultiSet (Pixel a) }
+  deriving (Eq, Ord, Show)
 
+diagonal :: a -> Pixel a
+diagonal a = Pixel a a
 
+transpose :: Pixel a -> Pixel a
+transpose (Pixel m n) = Pixel n m
+
+columnCollinear :: (Eq a) => Pixel a -> Pixel a -> Bool
+columnCollinear (Pixel _ c1) (Pixel _ c2) = c1 == c2
+
+rowCollinear :: (Eq a) => Pixel a -> Pixel a -> Bool
+rowCollinear (Pixel r1 _) (Pixel r2 _) = r1 == r2
+
+collinear :: (Eq a) => Pixel a -> Pixel a -> Bool
+collinear = liftA2 (||) <$> columnCollinear <*> rowCollinear
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,41 @@
+{-# LANGUAGE TemplateHaskell  #-}
+
+module Main where
+
+import           Control.Monad (void)
+import           Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+
+import           Data.Maxel
+
+genPixel :: Gen (Pixel Int)
+genPixel = do
+  x <- Gen.int Range.linearBounded
+  y <- Gen.int Range.linearBounded
+  return $ Pixel x y
+
+genDiagonal :: Gen (Pixel Int)
+genDiagonal =
+  diagonal <$> Gen.int Range.linearBounded
+
+-- A pixel 'p' is a diagonal exactly when 'transpose p = p'
+prop_transpose_diagonal :: Property
+prop_transpose_diagonal =
+  property $ do
+    a <- forAll genDiagonal
+    transpose a === a
+
+-- transpose . transpose = id
+prop_transpose_identity :: Property
+prop_transpose_identity =
+  property $ do
+    a <- forAll genPixel
+    transpose (transpose a) === a
+
+
+tests :: IO Bool
+tests = checkParallel $$(discover)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = void tests
